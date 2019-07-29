ccf22769b915ae4fc2d204ebb67473ee3fff003e
Ben D <benjamin.d928@icloud.com>
2019-07-29 14:39:26 +0100

    added routes in app

--- app.rb
+++ app.rb
@@ -2,7 +2,25 @@ require 'sinatra/base'
 require 'data_mapper_setup'
 class Bliss < Sinatra::Base
 
+get '/' do
 
+end
+
+get '/logon' do
+
+end
+
+get '/spaces' do
+
+end
+
+get '/spaces/new' do
+
+end
+
+get '/requests' do
+
+end
 
 
 run! if app_file == Bliss
