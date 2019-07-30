e378913e9aa0c0d1eb34559c2f07f942aa494e89
Ben D <benjamin.d928@icloud.com>
2019-07-30 11:53:43 +0100

    Added feature test page for viewing listings

---
+++ spec/features/listing_spaces_spec.rb
@@ -0,0 +1,8 @@
+feature 'listing available spaces' do
+  scenario 'the spaces are listed' do
+    visit('/')
+    expect(page).to have_content 'Loft Space'
+    expect(page).to have_content 'Cool'
+    expect(page).to have_content '100'
+  end
+end
