3f280c4e8a4de19a1f60a39d95e0260a8fb52b95
Kareem <kareem335@yahoo.co.uk>
2019-07-31 15:15:47 +0100

    authentication

--- spec/features/listing_spaces_spec.rb
+++ spec/features/listing_spaces_spec.rb
@@ -1,6 +1,16 @@
 feature 'listing available spaces' do
   scenario 'the spaces are listed' do
-    visit '/spaces'
+    User.create(
+      :user_name => "Someone",
+      :email => "something@someone.somewhat",
+      :password => "healthybananas",
+    )
+
+  visit('/sessions/new')
+  fill_in "email", with: "something@someone.somewhat"
+  fill_in "password", with: "healthybananas"
+  click_button("Sign in")
+
     expect(page).to have_content 'Loft Space'
     # expect(page).to have_content 'Cool'
     # expect(page).to have_content '100'
