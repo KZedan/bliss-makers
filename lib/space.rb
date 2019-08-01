405e12ef75b8e21d3c95b3fbd1b1ac80a99840fb
Ben D <benjamin.d928@icloud.com>
2019-08-01 17:00:45 +0100

    Added available dates method back in

--- lib/space.rb
+++ lib/space.rb
@@ -12,7 +12,6 @@ class Space
     property :available_to, Date
     belongs_to :user
 
-<<<<<<< HEAD
     def space_name
       return self[:space_name]
     end
@@ -24,11 +23,8 @@ class Space
     def price
       return self[:price]
     end
-  end
-=======
+    
     def available_dates
       @available_dates = Array(available_from...available_to).map {|date| date.strftime("%Y-%m-%d")}
     end
-
 end
->>>>>>> ad3dbbee7c063011e3a4378ddb7eb71c3f972e4d
