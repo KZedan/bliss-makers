c23fcc7d5773a0b4549104f2a2b2b20f4404b333
Student <student@Makerss-MacBook-Pro.local>
2019-08-01 12:26:47 +0100

    available dates stored in an array working

--- lib/space.rb
+++ lib/space.rb
@@ -10,20 +10,11 @@ class Space
     property :user_id, Integer
     property :available_from, Date
     property :available_to, Date
-    property :available_dates, Date
     belongs_to :user
 
 
-    def self.available_dates
-      @spaces = Space.all
-
-        @spaces.each do |space|
-           p available_from = space.available_from
-           p available_to = space.available_to
-           p date_range = available_from...available_to
-           available_dates = date_range.to_a
-        end
-        available_dates
+    def available_dates
+      @available_dates = Array(available_from...available_to).map {|date| date.strftime("%Y-%m-%d")}
     end
 
 end
--- spec/space_spec.rb
+++ spec/space_spec.rb
@@ -1,7 +1,18 @@
 describe Space do
   describe '.available_dates' do
+
+
+    space = Space.create(
+      :space_name => "Central appartment",
+      :description => "close to the station",
+      :price => "50",
+      :user_id => "1",
+      :available_from => "2019-08-01",
+      :available_to => "2019-08-03"
+    )
+
     it 'creates an array of available dates' do
-        expect(Space.available_dates).to eq '["2019-08-01", "2019-08-02", "2019-08-03"]'
+        expect(space.available_dates).to eq ["2019-08-01", "2019-08-02"]
     end
   end
 end
